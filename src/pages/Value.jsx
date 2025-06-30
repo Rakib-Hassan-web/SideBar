8d45d8cdabc01eabe7cbc16b2c512baecbc9e647
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-01 01:06:44 +0600

    complete but nedd 100% revise and practice again

--- src/pages/Value.jsx
+++ src/pages/Value.jsx
@@ -2,24 +2,37 @@ import React, { useState } from 'react'
 
 const Value = () => {
 
-const[value , setvalue ] =useState(0)
+const[value , setvalue ] =useState(1)
 
 
 const handleplus =()=>{
     setvalue(value+1)
 }
 
+const handleminus =()=>{
+    setvalue(value-1)
+    if(value<2){
+        alert('value 0 er upore')
+        setvalue(value==1)
+    }
+    else{
+        
+    }
+}
 
   return (
-    <div className=' ml-[400px] mt-[-555px] flex justify-center items-center gap-5'>
+    <div className=' ml-[400px] mt-[-555px] flex justify-center items-center gap-9'>
 
 
-   <h1 className='text-[40px]'>{value}</h1>
+<button  onClick={handleminus} className=' border-[2px] w-[125px] text-[33px] p-[3px] border-red-500 rounded-[8px] bg-red-400' >MINUS</button>
+
+
+   <h1 className='text-[50px]'>{value}</h1>
 
 
 
 
-<button  onClick={handleplus}  >Plus</button>
+<button  onClick={handleplus} className=' border-[2px] w-[95px] text-[33px] p-[3px] border-red-500 rounded-[8px] bg-red-400' >PLUS</button>
   
 
 
